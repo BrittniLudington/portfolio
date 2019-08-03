1787d01ecba18f2d07465a677cd40ed9892f4f12
BrittniLudington <brittniludington@gmail.com>
2019-08-02 19:26:12 -0700

    added new projects to list

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,22 @@
 export const projects =
 [
     {
+        repo: "https://github.com/BrittniLudington/kirby-fruit-run",
+        demo: "https://brittniludington.github.io/kirby-fruit-run/",
+        alt: "photo of kirby's fruit run",
+        name: "Kirby's Fruit Run",
+        src: "images/project-images/kirby-fruit-run.png",
+        description: "Game made for Kirby Jam 2019 on itch.io"
+    },
+    {
+        repo: "https://github.com/BrittniLudington/game-of-war",
+        demo: "https://game-of-war-git-master.bludington.now.sh/menu",
+        alt: "photo of game of war app",
+        name: "Game Of War",
+        src: "images/project-images/game-of-war.PNG",
+        description: "A digital version of the card game War. It's main feature is a save system that tracks the stats of a player and the current game"
+    },
+    {
         src : "images/project-images/quiz-app-small.PNG",
         alt : "photo of riddle quiz app",
         name: "Riddle Quiz App",
