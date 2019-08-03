7bd46c5d0be14f7528322b21794b408fde40da70
BrittniLudington <brittniludington@gmail.com>
2019-08-02 18:58:01 -0700

    made script for rendering project list

---
+++ js/projects.js
@@ -0,0 +1,19 @@
+export const projects =
+[
+    {
+        src : "images/project-images/quiz-app-small.PNG",
+        alt : "photo of riddle quiz app",
+        name: "Riddle Quiz App",
+        description: "A project made for the Thinkful boot camp. It is a quiz of ten riddles the user must solve.",
+        repo: "https://github.com/BrittniLudington/QuizApp",
+        demo: "https://brittniludington.github.io/QuizApp/index.html"
+    },
+    {
+        src : "images/project-images/time-catcher.PNG",
+        alt : "photo of time catcher",
+        name: "Time Catcher",
+        description: "Project made for the API capstone in Thinkful's boot camp. It takes a location and places each unit of current time inside a game. The goal is to find the seconds, minutes, and hours as fast as possible. Made using HTML, JQuery, and Javascript.",
+        repo: "https://github.com/BrittniLudington/time-and-weather-game-api-capstone",
+        demo: "https://brittniludington.github.io/time-and-weather-game-api-capstone/"
+    }
+]
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+import {projects} from "./projects.js";
+
 $(document).ready(function(){
     handleNavBar();
     handleSubmit();
+    renderProjects();
 });
 
 function handleNavBar()
@@ -21,4 +24,33 @@ function handleSubmit()
         event.preventDefault();
 
     });
+}
+
+function renderProjects()
+{
+    /*  FORMAT
+                <article class="sansserif">
+                <div class="imageHolder">
+                    <img class = "thumbnail" src = "images/project-images/quiz-app-small.PNG" alt="photo of riddle quiz app">
+                </div>
+                <div class = "project-contents">
+                    <h2>Riddle Quiz App</h2>
+                    <p>A project made for the Thinkful boot camp. It is a quiz of ten riddles
+                            the user must solve. Made using HTML and Javascript.</p>
+                    <a href="https://brittniludington.github.io/QuizApp/index.html">Demo</a>
+                    <a href="https://github.com/BrittniLudington/QuizApp">Repository</a>
+                </div>
+            </article>
+    */
+   $("#projects").prepend('<h1 class="merriweather">Projects</h1>');
+    for(let i = 0; i < projects.length; i++)
+   {
+       let html = '<article class="sansserif"><div class="imageHolder"><img class="thumbnail" src=' + projects[i].src +
+        ' alt=' + projects[i].alt + '></div> ' + 
+        '<div class = "project-contents"> <h2>' + projects[i].name + '</h2><p>' + projects[i].description +  '</p>' 
+        + '<a href = ' + projects[i].demo + '>Demo</a><a href = ' + projects[i].repo + '>Repository</a><div></article>';
+        html = $.parseHTML(html);
+        $("#projects").append(html);
+   }
+   
 }
