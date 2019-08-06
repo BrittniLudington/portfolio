562b0884d475c7ba0151a7f88799e1baedd16a19
BrittniLudington <brittniludington@gmail.com>
2019-08-06 14:41:08 -0700

    added techonology used

--- js/projects.js
+++ js/projects.js
@@ -6,7 +6,17 @@ export const projects =
         alt: "photo of kirby's fruit run",
         name: "Kirby's Fruit Run",
         src: "./images/project-images/kirby-fruit-run.PNG",
-        description: "Game made for Kirby Jam 2019 on itch.io under my account"
+        description: "Game made for Kirby Jam 2019 on itch.io under my account.",
+        technology: ["Javascript","HTML","CSS","Gimp"]
+    },
+    {
+        repo: "https://github.com/BrittniLudington/messenger",
+        demo: "https://messenger.bludington.now.sh/login",
+        src:"./images/project-images/messenger.PNG",
+        alt: "photo of messenger app",
+        name: "Private Messenger",
+        description: "A private messaging application that lets users create accounts and send messages to specified users.",
+        technology: ["React js", "Node.js", "Javascript", "PostgreSQL","CSS"]
     },
     {
         repo: "https://github.com/BrittniLudington/game-of-war",
@@ -14,6 +24,7 @@ export const projects =
         alt: "photo of game of war app",
         name: "Game Of War",
         src: "images/project-images/game-of-war.PNG",
+        technology:["React js", "Node.js","Javascript","PostgreSQL","CSS","Gimp"],
         description: "A digital version of the card game War. It's main feature is a save system that tracks the stats of a player and the current game"
     },
     {
@@ -22,7 +33,8 @@ export const projects =
         name: "Riddle Quiz App",
         description: "A project made for the Thinkful boot camp. It is a quiz of ten riddles the user must solve.",
         repo: "https://github.com/BrittniLudington/QuizApp",
-        demo: "https://brittniludington.github.io/QuizApp/index.html"
+        demo: "https://brittniludington.github.io/QuizApp/index.html",
+        technology: ["Javascript","HTML","CSS","Gimp"]
     },
     {
         src : "images/project-images/time-catcher.PNG",
@@ -30,6 +42,7 @@ export const projects =
         name: "Time Catcher",
         description: "Project made for the API capstone in Thinkful's boot camp. It takes a location and places each unit of current time inside a game. The goal is to find the seconds, minutes, and hours as fast as possible. Made using HTML, JQuery, and Javascript.",
         repo: "https://github.com/BrittniLudington/time-and-weather-game-api-capstone",
-        demo: "https://brittniludington.github.io/time-and-weather-game-api-capstone/"
+        demo: "https://brittniludington.github.io/time-and-weather-game-api-capstone/",
+        technology: ["Javascript","JQuery","HTML","CSS","Gimp"]
     }
 ]
