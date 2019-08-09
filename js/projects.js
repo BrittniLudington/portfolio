3156b8b5a31741270686a33065fccb28f4b10c30
BrittniLudington <brittniludington@gmail.com>
2019-08-08 20:44:01 -0700

    fixed project descriptions

--- js/projects.js
+++ js/projects.js
@@ -31,7 +31,7 @@ export const projects =
         src : "images/project-images/quiz-app-small.PNG",
         alt : "photo of riddle quiz app",
         name: "Riddle Quiz App",
-        description: "A project made for the Thinkful boot camp. It is a quiz of ten riddles the user must solve.",
+        description: "A small quiz app that tests the user's thinking skills. The goal is to answer all ten questions correctly.",
         repo: "https://github.com/BrittniLudington/QuizApp",
         demo: "https://brittniludington.github.io/QuizApp/index.html",
         technology: ["Javascript","HTML","CSS","Gimp"]
@@ -40,7 +40,7 @@ export const projects =
         src : "images/project-images/time-catcher.PNG",
         alt : "photo of time catcher",
         name: "Time Catcher",
-        description: "Project made for the API capstone in Thinkful's boot camp. It takes a location and places each unit of current time inside a game. The goal is to find the seconds, minutes, and hours as fast as possible. Made using HTML, JQuery, and Javascript.",
+        description: "A video game that uses location and weather data. It takes a location and places each unit of current time inside a game. The goal is to find the seconds, minutes, and hours as fast as possible. Made using HTML, JQuery, and Javascript.",
         repo: "https://github.com/BrittniLudington/time-and-weather-game-api-capstone",
         demo: "https://brittniludington.github.io/time-and-weather-game-api-capstone/",
         technology: ["Javascript","JQuery","HTML","CSS","Gimp"]
