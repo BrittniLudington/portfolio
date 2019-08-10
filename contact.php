5f3c25d2713e8dd6cbb0246ac44dc69f54054bd4
BrittniLudington <brittniludington@gmail.com>
2019-08-09 21:06:17 -0700

    more text formatting

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
+    echo "HELOOO";
     if($_POST["name"] && $_POST["email"] && $_POST["subject"] && $_POST["message"])
     {
         mail("brittniludington@gmail.com",$_POST["subject"],$_POST["message"],"From: " . $_POST["email"]);
